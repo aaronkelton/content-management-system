71650bb517c714cc419ec52ec7aaec7c21352b65
chemturion <aaron.k.kelton@gmail.com>
2017-09-11 14:38:48 -0500

    explicit demo controller render on #index & #hello

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -3,8 +3,10 @@ class DemoController < ApplicationController
   layout false
 
   def index
+    render template: 'demo/index.html.erb'
   end
 
   def hello
+    render template: 'demo/hello.html.erb'
   end
 end
