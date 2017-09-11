ce341c3a4f15d32cd41025e6fc6239a2956692ad
chemturion <aaron.k.kelton@gmail.com>
2017-09-11 12:19:56 -0500

    added SIMPLE, DEFAULT, and ROOT routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,17 @@
 Rails.application.routes.draw do
-  get 'demo/index'
+
+  # ROOT Route
+  # match '/', to: 'demo#index', via: :get #longhand MATCH-y form
+  root to: 'demo#index' # shorter sans MATCH
+  # root 'demo#index' # super-short-y
+
+  # SIMPLE MATCH Route
+  # get 'demo/index' # shorthand version
+  match 'demo/index', to: 'demo#index', via: :get # longhand version
+
+  # DEFAULT Route, parens indicate optional
+  # may be deprecated in future Rails versions
+  # get ':controller(/:action(/:id))'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
