6e70171f0d2cb5ac3f29c1fd6414030652381bba
chemturion <aaron.k.kelton@gmail.com>
2017-09-11 17:32:28 -0500

    pages migration and model-subject association

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,2 +1,3 @@
 class Page < ApplicationRecord
+  belongs_to :subject
 end
--- db/migrate/20170911220633_create_pages.rb
+++ db/migrate/20170911220633_create_pages.rb
@@ -1,8 +1,16 @@
 class CreatePages < ActiveRecord::Migration[5.1]
-  def change
+  def up
     create_table :pages do |t|
-
+      t.string :name, index: true
+      # t.index :name # using `index: true` instead
+      t.integer :permalink
+      t.integer :position
+      t.boolean :visible
+      t.belongs_to :subject, foreign_key: true
       t.timestamps
     end
+  end
+  def down
+    drop_table :pages
   end
 end
