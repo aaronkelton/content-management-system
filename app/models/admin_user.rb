885676625114797f708cc5ecba3e01aa10829e02
chemturion <aaron.k.kelton@gmail.com>
2017-09-12 16:31:13 -0500

    admin_user ~ section has_many through: :section_edits

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -3,6 +3,7 @@ class AdminUser < ApplicationRecord
   ##### ASSOCIATIONS #####
   has_and_belongs_to_many :pages
   has_many :section_edits
+  has_many :sections, through: :section_edits
   ########################
 
   ################################
