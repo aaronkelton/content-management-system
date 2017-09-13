e45743ef6418c76f2b234476c00057faf642cb08
chemturion <aaron.k.kelton@gmail.com>
2017-09-12 20:10:08 -0500

    wrote create conditional POST, save & redirect OR render 'new'

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -6,6 +6,17 @@ class SubjectsController < ApplicationController
   end
 
   def create
+    # Instantiate a new object using form params
+    @subject = Subject.new(params[:subject])
+    # Save the object
+    if @subject.save
+      # If save succeeds, redirect to the index action
+      redirect_to subjects_path
+    else
+      # If save fails, redisplay the form so user can fix problems
+      render template: 'new'
+    end
+
   end
   #### END CREATE ####
 
