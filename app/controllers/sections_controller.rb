6b3f97c31c85795702b3230e05d4bae599a58a85
chemturion <aaron.k.kelton@gmail.com>
2017-09-13 19:09:45 -0500

    updated sections controller to match Skoglynd

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -4,15 +4,11 @@ class SectionsController < ApplicationController
   end
 
   def create
-    # Instantiate a new object using form params
     @section = Section.new(section_params)
-    # Save the object
     if @section.save
-      # If save succeeds, redirect to the index action
       flash[:notice] = "Section created successfully!"
       redirect_to sections_path
     else
-      # If save fails, redisplay the form so user can fix problems
       render 'new'
     end
   end
@@ -30,15 +26,11 @@ class SectionsController < ApplicationController
   end
 
   def update
-    # Find a new object using form params
     @section = Section.find(params[:id])
-    # Update the object
     if @section.update_attributes(section_params)
-      # If save succeeds, redirect to the show action
       flash[:notice] = "Section upated successfully!"
       redirect_to section_path(@section)
     else
-      # If save fails, redisplay the form so user can fix problems
       render template: 'edit'
     end
   end
@@ -50,12 +42,12 @@ class SectionsController < ApplicationController
   def destroy
     @section = Section.find(params[:id])
     @section.destroy
-    flash[:notice] = "Section '#{@section.name}' destroyed successfully!"
+    flash[:notice] = "Section destroyed successfully!"
     redirect_to sections_path
   end
 
   private
   def section_params
-    params.require(:section).permit(:name, :position, :visible, :content_type, :content)
+    params.require(:section).permit(:name, :position, :visible, :content_type, :content, :page_id)
   end
 end
