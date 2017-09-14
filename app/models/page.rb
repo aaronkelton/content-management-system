f494a9cf1f94be974524729065d8540f603fe73c
chemturion <aaron.k.kelton@gmail.com>
2017-09-13 19:03:45 -0500

    added scopes to match subject model

--- app/models/page.rb
+++ app/models/page.rb
@@ -8,5 +8,8 @@ class Page < ApplicationRecord
 
   ################# END ASSOCIATIONS ################
 
+  scope :visible, lambda { where(visible: true) }
+  scope :invisible, lambda { where(visible: false) }
   scope :sorted, lambda { order(position: :ASC) }
+  scope :newest_first, lambda { order(created_at: 'DESC') }
 end
