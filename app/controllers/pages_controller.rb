3303451be214eb8d8c605df2d2bf97d51cce0f7d
chemturion <aaron.k.kelton@gmail.com>
2017-09-13 19:07:59 -0500

    updated pages controller to match Skoglynd

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,14 +1,11 @@
 class PagesController < ApplicationController
   def new
-    @page = Page.new(name: 'Default page')
+    @page = Page.new
   end
 
   def create
-    # Instantiate a new object using form params
     @page = Page.new(page_params)
-    # Save the object
     if @page.save
-      # If save succeeds, redirect to the index action
       flash[:notice] = "Page created successfully!"
       redirect_to pages_path
     else
@@ -29,15 +26,11 @@ class PagesController < ApplicationController
   end
 
   def update
-    # Find a new object using form params
     @page = Page.find(params[:id])
-    # Update the object
     if @page.update_attributes(page_params)
-      # If save succeeds, redirect to the show action
       flash[:notice] = "Page upated successfully!"
       redirect_to page_path(@page)
     else
-      # If save fails, redisplay the form so user can fix problems
       render template: 'edit'
     end
   end
@@ -49,12 +42,12 @@ class PagesController < ApplicationController
   def destroy
     @page = Page.find(params[:id])
     @page.destroy
-    flash[:notice] = "Page '#{@page.name}' destroyed successfully!"
+    flash[:notice] = "Page destroyed successfully!"
     redirect_to pages_path
   end
 
   private
     def page_params
-      params.require(:page).permit(:name, :visible, :position, :permalink)
+      params.require(:page).permit(:name, :visible, :position, :permalink, :subject_id)
     end
 end
