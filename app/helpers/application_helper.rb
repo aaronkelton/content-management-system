d1c1a8d0c929450498d329d31089f7d2383dfd8a
chemturion <aaron.k.kelton@gmail.com>
2017-09-14 21:48:48 -0500

    created error logic and view helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,8 @@
 module ApplicationHelper
 
+  def error_messages_for(object)
+    render partial: 'application/error_messages', locals: { object: object }
+  end
   def status_tag(boolean, options={})
     options[:true_text]  ||= ''
     options[:false_text] ||= ''
