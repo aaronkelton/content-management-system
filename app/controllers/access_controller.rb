5d4326028e8545999df3e652aa503f61d2835512
chemturion <aaron.k.kelton@gmail.com>
2017-09-15 15:44:13 -0500

    wrote access#attempt_login logic

--- app/controllers/access_controller.rb
+++ app/controllers/access_controller.rb
@@ -11,7 +11,22 @@ class AccessController < ApplicationController
   end
 
   def attempt_login
-    #code
+    if params[:username].present? && params[:password].present?
+      found_user = AdminUser.where(username: params[:username]).first
+      if found_user
+        authorized_user = found_user.authenticate(params[:password])
+      end
+    end
+
+    if authorized_user
+      session[:user_id] = authorized_user.id
+      flash[:notice] = "You are now logged in."
+      redirect_to admin_path
+    else
+      flash.now[:notice] = "Invalid username/password combination."
+      render 'login'
+    end
+
   end
 
   def logout
