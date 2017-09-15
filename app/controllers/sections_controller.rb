7dd0e0149f7ef810574f44a17d46acc6f60bc7f6
chemturion <aaron.k.kelton@gmail.com>
2017-09-15 13:05:08 -0500

    moved @pages and @section_count to before_action; made conditional @section_count for #new and #create

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -2,10 +2,11 @@ class SectionsController < ApplicationController
 
   layout 'admin'
 
+  before_action :find_and_sort_pages, only: [:new, :create, :edit, :update]
+  before_action :set_section_count, only: [:new, :create, :edit, :update]
+
   def new
     @section = Section.new(name: 'Default')
-    @section_count = Section.count + 1
-    @pages = Page.sorted
   end
 
   def create
@@ -14,8 +15,6 @@ class SectionsController < ApplicationController
       flash[:notice] = "Section created successfully!"
       redirect_to sections_path
     else
-      @section_count = Section.count + 1
-      @pages = Page.sorted
       render 'new'
     end
   end
@@ -30,8 +29,6 @@ class SectionsController < ApplicationController
 
   def edit
     @section = Section.find(params[:id])
-    @section_count = Section.count
-    @pages = Page.sorted
   end
 
   def update
@@ -40,8 +37,6 @@ class SectionsController < ApplicationController
       flash[:notice] = "Section upated successfully!"
       redirect_to section_path(@section)
     else
-      @section_count = Section.count
-      @pages = Page.sorted
       render template: 'edit'
     end
   end
@@ -60,5 +55,14 @@ class SectionsController < ApplicationController
   private
   def section_params
     params.require(:section).permit(:name, :position, :visible, :content_type, :content, :page_id)
+  end
+
+  def find_and_sort_pages
+    @pages = Page.sorted
+  end
+
+  def set_section_count
+    @section_count = Section.count
+    @section_count += 1 if params[:action] == 'new' || params[:action] == 'create'
   end
 end
