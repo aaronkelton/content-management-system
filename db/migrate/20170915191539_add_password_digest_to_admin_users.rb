c580007950adcd10c19c8d5d39517cc5b5f92296
chemturion <aaron.k.kelton@gmail.com>
2017-09-15 14:21:05 -0500

    changed hashed_password attr to password_digest for admin_users table

---
+++ db/migrate/20170915191539_add_password_digest_to_admin_users.rb
@@ -0,0 +1,10 @@
+class AddPasswordDigestToAdminUsers < ActiveRecord::Migration[5.1]
+  def up
+    remove_column "admin_users", "hashed_password"
+    add_column "admin_users", "password_digest", :string
+  end
+  def down
+    remove_column "admin_users", "password_digest"
+    add_column "admin_users", "hashed_password", :string, limit: 40
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170912164805) do
+ActiveRecord::Schema.define(version: 20170915191539) do
 
   create_table "admin_users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "first_name", limit: 25
     t.string "last_name", limit: 50
     t.string "email", limit: 100, default: "", null: false
     t.string "username", limit: 25
-    t.string "hashed_password", limit: 40
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
     t.index ["username"], name: "index_admin_users_on_username"
   end
 
