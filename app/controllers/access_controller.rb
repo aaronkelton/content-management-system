8593cad6a6a598a0b088184df185d3b10638ad86
chemturion <aaron.k.kelton@gmail.com>
2017-09-15 15:01:01 -0500

    wrote #attempt_login and #logout method stubs

--- app/controllers/access_controller.rb
+++ app/controllers/access_controller.rb
@@ -1,7 +1,17 @@
 class AccessController < ApplicationController
   def menu
+    # display text and links
   end
 
   def login
+    # login form
+  end
+
+  def attempt_login
+    #code
+  end
+
+  def logout
+    #code
   end
 end
