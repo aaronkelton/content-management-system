409581fa3fa20fe1ece7d6b667c9e9cc1ca25eeb
chemturion <aaron.k.kelton@gmail.com>
2017-09-16 23:49:58 -0500

    wrote Public controller layout and logic

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -1,7 +1,17 @@
 class PublicController < ApplicationController
+
+  layout 'public'
+
   def index
+    # intro text
   end
 
   def show
+    @page = Page.visible.where(permalink: params[:permalink]).first
+    if page.nil?
+      redirect_to root_path
+    else
+      # display the page content using show.html.erb
+    end
   end
 end
